ae9776601d35e3b697f87f7bb900e277979d491f
aryndam9 <39860536+aryndam9@users.noreply.github.com>
2023-10-12 23:31:57 +0530

    annotation

--- geoai.py
+++ geoai.py
@@ -2,6 +2,11 @@ import streamlit as st
 from streamlit_image_comparison import image_comparison
 import rasterio
 import os
+from glob import glob
+import pandas as pd
+from streamlit_image_annotation import detection
+import json
+import cv2
 
 # def geotiff_to_jpg(tif_filename):
 #     with rasterio.open(tif_filename) as infile:
@@ -92,6 +97,7 @@ def extract_zip(zip_file):
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
         zip_ref.extractall("uploaded_data")
 
+
 # Allow users to upload a zip file
 uploaded_zip = st.file_uploader("Upload a ZIP file containing dataset:", type=["zip"])
 
@@ -106,6 +112,94 @@ if uploaded_zip:
         # For example, list the extracted files
         extracted_files = os.listdir("uploaded_data")
         st.write("Extracted files:", extracted_files)
+
+
+label_list = ['black patch']
+image_path_list = glob('images/*.jpg')
+if 'result_dict' not in st.session_state:
+    result_dict = {}
+    for img in image_path_list:
+        result_dict[img] = {'bboxes': [], 'labels': []}
+    st.session_state['result_dict'] = result_dict.copy()
+
+# with next and prev button to cycle through the images two columns
+col1,_, col2 = st.columns(3)
+with col1:
+    prev_button = st.button('Previous')
+with col2:
+    next_button = st.button('Next')
+
+if prev_button:
+    if st.session_state['num_page'] > 0:
+        st.session_state['num_page'] -= 1
+elif next_button:
+    if st.session_state['num_page'] < len(image_path_list) - 1:
+        st.session_state['num_page'] += 1
+else:
+    if 'num_page' not in st.session_state:
+        st.session_state['num_page'] = 0
+    else:
+        st.session_state['num_page'] = st.session_state['num_page']
+
+
+num_page = st.session_state['num_page']
+
+target_image_path = image_path_list[num_page]
+
+new_labels = detection(image_path=target_image_path,
+                    bboxes=st.session_state['result_dict'][target_image_path]['bboxes'], 
+                    labels=st.session_state['result_dict'][target_image_path]['labels'], 
+                    label_list=label_list, key=target_image_path)
+
+if new_labels is not None:
+    st.session_state['result_dict'][target_image_path]['bboxes'] = [v['bbox'] for v in new_labels]
+    st.session_state['result_dict'][target_image_path]['labels'] = [v['label_id'] for v in new_labels]
+
+# add a text box saying 'annotation completed for 1/2 images'
+if st.session_state['num_page'] is not None:
+    # blue color for the text box
+    # Set the background color of the box to blue
+    st.markdown(
+    """
+    <style>
+    .blue-box {
+        background-color: rgba(52, 152, 219, 0.3); /* Blue with 60% opacity */
+        padding: 10px;
+        border-radius: 10px;
+        border: 2px solid #6A87B3; /* Dark blue border */
+        width: 60%; /* Set the width to 30% of the container */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+    # Create the blue-colored box with the text inside
+    st.markdown('<div class="blue-box">❕Annotation completed for {}/{} images</div>'.format(num_page+1  , len(image_path_list)), unsafe_allow_html=True)
+
+# space
+st.write('')
+annotation_json = st.session_state['result_dict']
+# length of non-empty bboxes and labels in the annotation json 
+count_file = 0
+count_label = 0
+for img in annotation_json:
+    if len(annotation_json[img]['bboxes']) > 0 and len(annotation_json[img]['labels']) > 0:
+        count_file += 1
+        count_label += 1
+    else:
+        count_file += 1
+
+
+
+
+# download button to the json file
+if count_file == count_label:
+    st.download_button(label='Download Annotation JSON', data=json.dumps(annotation_json), file_name='annotation.json', mime='application/json')
+    for nimg in range(st.session_state['num_page']):
+        # display the image first
+        st.image(image_path_list[nimg])
+
 
 
 
